95de0eff33c62b62cad470cc98863b96a7ba88a7
Rakshith2605 <rakshith.officialmail@gmail.com>
2026-04-16 14:38:01 -0500

    deployement error fix-2

--- src/app/(learn)/layout.tsx
+++ src/app/(learn)/layout.tsx
@@ -1,6 +1,8 @@
 import { fetchCurriculum } from "@/lib/db-curriculum";
 import { LearnLayoutClient } from "@/components/layout/LearnLayoutClient";
 
+export const dynamic = "force-dynamic";
+
 export default async function LearnLayout({ children }: { children: React.ReactNode }) {
   const curriculum = await fetchCurriculum();
 
